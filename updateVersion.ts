0929cfead55a12b410972ba88cd1d6ad3008fa23
Jacob Lamb <jacobtlamb@hey.com>
2024-02-20 14:12:23 -0800

    Update dep

--- updateVersion.ts
+++ updateVersion.ts
@@ -1,14 +1,14 @@
-import { fsx } from "fsx-node";
+import { hfs } from "@humanfs/node";
 async function updateVersion() {
 	const path = "./package.json";
-	const content = await fsx.json(path);
+	const content = await hfs.json(path);
 	const current_date = new Date().toISOString().slice(0, 10).replace(/-/g, ".");
 	const newVersion = content.version.replace(
 		/(\d{4}\.\d{2}\.\d{2})/,
 		current_date,
 	);
 	content.version = newVersion;
-	await fsx.write(path, JSON.stringify(content, null, 2));
+	await hfs.write(path, JSON.stringify(content, null, 2));
 	console.log(`Updated version to ${newVersion}`);
 }
 
