36b3560f1a761885dd8c5371470124a50aa4ad97
Jacob Lamb <jacobtlamb@hey.com>
2024-04-02 08:58:52 -0700

    Tiny tweaks

--- updateVersion.ts
+++ updateVersion.ts
@@ -2,10 +2,10 @@ import { hfs } from "@humanfs/node";
 async function updateVersion() {
 	const path = "./package.json";
 	const content = await hfs.json(path);
-	const current_date = new Date().toISOString().slice(0, 10).replace(/-/g, ".");
+	const currentDate = new Date().toISOString().slice(0, 10).replace(/-/g, ".");
 	const newVersion = content.version.replace(
 		/(\d{4}\.\d{2}\.\d{2})/,
-		current_date,
+		currentDate,
 	);
 	content.version = newVersion;
 	await hfs.write(path, JSON.stringify(content, null, 2));
